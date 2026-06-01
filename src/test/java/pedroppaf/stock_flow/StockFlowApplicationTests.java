d74ddb616f5471c50e573798e3e86e623e2a395d
Pedro <pedroppaf@gmail.com>
2026-06-01 16:03:32 -0300

    commit inicial

---
+++ src/test/java/pedroppaf/stock_flow/StockFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroppaf.stock_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
